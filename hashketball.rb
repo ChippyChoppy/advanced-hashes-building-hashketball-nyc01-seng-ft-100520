704baf16e75e43f7d8452fc4fa235490a71efaf1
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 05:11:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,21 @@
 # Write your code here!
 {:home => 
-  {:team_name => " ",
-  :colors => ["", ""]
-  :players => [{ player_name: "",
-  number: 
-    
-  }]
+  {:team_name => "Brooklyn Nets",
+  :colors => ["Black", "White"]]
+  :players => [
+    { :player_name => "",
+    :number => ,
+    :shoe => ,
+    :points => ,
+    :rebounds => ,
+    :assists => ,
+    :steals => ,
+    :blocks => ,
+    :slam_dunks => 
+    }]
   }
 
 :away
+}
 
 def game_hash
